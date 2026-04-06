f393b1b9947f8c95a3016aa978664a0f1cca8c9c
Mohannad Ali <Mohannad7Ali@outlook.com>
2026-04-06 11:50:13 +0300

    chore: initialize nextjs project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  i18n: {
+    locales: ['ar'],
+    defaultLocale: 'ar',
+  },
 };
 
 export default nextConfig;
