506f65cffa39b0e3ce0e1fa03d5826cb896e4f8a
Mohannad Ali <Mohannad7Ali@outlook.com>
2026-04-06 12:48:45 +0300

    chore: setup Prisma 7 with PostgreSQL

---
+++ lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@/lib/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg'; // استيراد Pool ضروري هنا
+import 'dotenv/config';
+
+// 1. إنشاء الـ Pool الخاص بمكتبة pg
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // استخدم DATABASE_URL للـ Docker المحلي
+});
+
+// 2. ربط الـ Pool بمحول Prisma
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+
+// 3. تشغيل العميل مع المحول
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ['query', 'warn', 'error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
