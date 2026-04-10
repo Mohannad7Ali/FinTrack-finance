daaf267b3dc7815edce573a5f16d403b22d11322
Mohannad Ali <Mohannad7Ali@outlook.com>
2026-04-10 18:12:48 +0300

    build summary card component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import SummaryCard from '@/components/dashboard/SummaryCard';
+
 export default function Dashboard() {
 	return (
 		<main className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
 			<h1 className="text-3xl font-bold">مرحباً بك في نظام إدارة مالية</h1>
+			<SummaryCard title="any" value={456} color="emerlad" description="fsdgfhdkjsfh" />
 		</main>
 	);
 }
---
+++ components/dashboard/SummaryCard.tsx
@@ -0,0 +1,19 @@
+type SummaryCardProps = {
+	title: string;
+	value: number;
+	color: 'emerlad' | 'red';
+	description?: string;
+};
+
+const SummaryCard = ({ title, value, color, description }: SummaryCardProps) => {
+	const colorClass = color === 'emerlad' ? 'text-emerald-500' : 'text-red-500';
+	return (
+		<div className="relative groub overflow-hidden border border-white/10 bg-white/5 backdrob-blur-xl p-4 space-y-1 transition-all duration-200 hover:-translate-y-1 hover:border-emerald-400/70 ">
+			<h2 className="text-sm font-medium text-slate-200">{title}</h2>
+			<p className={`text-2xl font-bold ${colorClass}`}>R$ {value.toFixed(2)}</p>
+			<p className="text-slate-400 text-[11px]">{description}</p>
+		</div>
+	);
+};
+
+export default SummaryCard;
